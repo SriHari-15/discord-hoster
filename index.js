a2f761f347968b9e87851b7533e7e33e1eddd6bf
SriHari-15 <sriharidev1508@gmail.com>
2021-05-15 10:12:07 +0530

    First Commit

--- index.js
+++ index.js
@@ -4,8 +4,28 @@ const client = new Discord.Client()
 require('dotenv-flow').config()
 const token = process.env.TOKEN
 
+let clientName = '';
+
 client.on('ready', () => {
-    console.log(`${client.user.tag} is active!`)
+    clientName = client.user.tag
+    console.log(`${clientName} is active!`)
+
+    client.user.setPresence({
+        activity:{
+            name: 'Spotify',
+            type: 'LISTENING'
+        },
+        status: 'idle',
+        afk: true
+    }).then(console.log('Successfully Set Presence!')).catch(console.error)
 })
 
 client.login(token)
+
+//Server
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Discord Hoster is Running...');
+});
+server.listen(3000);
